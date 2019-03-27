d6f465143b56da0cd913b443438a9d9dd03dc785
lehungio <me@lehungio.com>
2019-03-27 15:10:49 +0900

    global config

--- setup.js
+++ setup.js
@@ -11,9 +11,11 @@ const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup')
 module.exports = async function() {
   console.log(chalk.green('Setup Puppeteer'))
   const browser = await puppeteer.launch({
-    headless: true,
-    slowMo: process.env.SLOWMO === "1" ? 500 : [],
-    args: process.env.SANDBOX === "0" ? ["--no-sandbox", '--disable-setuid-sandbox'] : [],
+    headless: process.env.DEBUG === "1" ? false : true,
+    slowMo: process.env.DEBUG === "1" ? 500 : 0,
+    devtools: process.env.CONSOLE === "1" ? true : false,
+    dumpio: process.env.DEBUG === "1" ? true : false,
+    args: process.env.DEBUG === "1" ? [] : ["--no-sandbox", '--disable-setuid-sandbox']
   })
   // This global is not available inside tests but only in global teardown
   global.__BROWSER_GLOBAL__ = browser
